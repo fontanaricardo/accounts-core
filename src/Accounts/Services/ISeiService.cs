7e8084f3c7ae5d8d77d948bb024b2127b1c0c953
Ricardo Lopes Bez Fontana <ricardo.fontana@joinville.sc.gov.br>
2017-02-24 11:14:35 -0300

    Added person requests to SeiServices

--- src/Accounts/Services/ISeiService.cs
+++ src/Accounts/Services/ISeiService.cs
@@ -12,5 +12,17 @@ namespace Accounts.Services
         void CreateProtocol(Person person);
 
         void ReopenProtocol(string protocol);
+
+        void ChangePasswordSei(Person person, string password, bool revokeSign = false);
+
+        Person GetSeiPersonBy(long id);
+
+        Person GetSeiPersonBy(string email);
+
+        void CreateOrUpdateSeiUser(Person person, string password);
+
+        void UpdateEletronicSignatureStatus(Person person);
+
+        bool SignatureIsAproved(Person person);
     }
 }
--- src/Accounts/Services/SeiService.cs
+++ src/Accounts/Services/SeiService.cs
@@ -3,6 +3,7 @@ namespace Accounts.Services
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Net.Http;
     using System.Text;
     using Accounts.Models;
@@ -122,6 +123,203 @@ namespace Accounts.Services
             values.Add(new KeyValuePair<string, string>("IdServico", _configuration["Servico"]));
 
             var response = ExtendableType.Post(_configuration["VirtualUrl"] + "/api/Seiprotocolos/Reabrir", values);
+        }
+
+        /// <summary>
+        /// Changes user's password in SEI ignoring infrastructure exceptions.
+        /// </summary>
+        /// <remarks>
+        /// If person.SeiId is null do nothing.
+        /// </remarks>
+        /// <param name="person">Person who will have his password changed.</param>
+        /// <param name="password">New user password.</param>
+        /// <param name="revokeSign">If true revoke user's signature.</param>
+        public void ChangePasswordSei(Person person, string password, bool revokeSign = false)
+        {
+            if (person.SeiId == null)
+            {
+                return;
+            }
+
+            var values = new List<KeyValuePair<string, string>>
+            {
+                    new KeyValuePair<string, string>("token", _configuration["Token"]),
+                    new KeyValuePair<string, string>("id_usuario", person.SeiId.ToString()),
+                    new KeyValuePair<string, string>("nova_senha", password)
+            };
+
+            if (revokeSign)
+            {
+                new KeyValuePair<string, string>("status", "P");
+            }
+
+            byte[] response = ExtendableType.Post(_configuration["UrlSei"] + "/pmj/cadastro_usuario_externo_senha.php", values);
+
+            var result = JsonConvert.DeserializeObject<dynamic>(Encoding.GetEncoding(_configuration["SeiEncoding"]).GetString(response));
+
+            if (result.status != 1)
+            {
+                throw new Exception("Erro ao alterar a senha no SEI");
+            }
+
+            if (revokeSign)
+            {
+                person.EletronicSignatureStatus = EletronicSignatureStatus.Unsolicited;
+            }
+        }
+
+        /// <summary>
+        /// Returns the person object from the SEI data. Use the service /pmj/cadastro_usuario_externo_consulta.php
+        /// </summary>
+        /// <param name="id">SEI user's code.</param>
+        /// <returns>Object with person SEI data.</returns>
+        public Person GetSeiPersonBy(long id)
+        {
+            return GetSeiPersonBy("id_usuario", id.ToString());
+        }
+
+        /// <summary>
+        /// Returns the person object from the SEI data. Use the service /pmj/cadastro_usuario_externo_consulta.php
+        /// </summary>
+        /// <param name="email">SEI user's e-mail.</param>
+        /// <returns>Object with person SEI data.</returns>
+        public Person GetSeiPersonBy(string email)
+        {
+            return GetSeiPersonBy("email", email);
+        }
+
+        /// <summary>
+        /// Create or update an SEI user.
+        /// </summary>
+        /// <param name="person">Person to be created on the SEI.</param>
+        /// <param name="password">SEI user's password.</param>
+        public void CreateOrUpdateSeiUser(Person person, string password)
+        {
+            if (person.Phones == null || person.Phones.Count() == 0)
+            {
+                throw new ArgumentNullException(null, "Deve ser informado o número de telefone");
+            }
+
+            if (person.Address == null)
+            {
+                throw new ArgumentNullException("Address", "Endereço não pode ficar em branco.");
+            }
+
+            // Token to service authentication
+            List<KeyValuePair<string, string>> values = new List<KeyValuePair<string, string>>();
+
+            values.Add(new KeyValuePair<string, string>("token", _configuration["Token"]));
+
+            Person seiPerson = GetSeiPersonBy(person.Email);
+
+            if (seiPerson != null)
+            {
+                if (!person.CPF.Equals(seiPerson.CPF))
+                {
+                    throw new ArgumentException("Este e-mail está relacionado a outro CPF no SEI");
+                }
+                else
+                {
+                    person.SeiId = seiPerson.SeiId;
+                }
+            }
+
+            if (person.SeiId != null)
+            {
+                values.Add(new KeyValuePair<string, string>("valores[id_usuario]", person.SeiId.ToString()));
+            }
+
+            // PHP's vector format
+            values.Add(new KeyValuePair<string, string>("valores[nome]", person.Name));
+            values.Add(new KeyValuePair<string, string>("valores[cpf]", person.CPF));
+            values.Add(new KeyValuePair<string, string>("valores[rg]", person.RG));
+            values.Add(new KeyValuePair<string, string>("valores[orgao_expedidor]", person.Dispatcher));
+            values.Add(new KeyValuePair<string, string>("valores[telefone]", person.PhoneLastUpdated));
+            values.Add(new KeyValuePair<string, string>("valores[endereco]", person.Address.StreetAndNumber));
+            values.Add(new KeyValuePair<string, string>("valores[bairro]", person.Address.District));
+            values.Add(new KeyValuePair<string, string>("valores[cidade]", person.Address.City));
+            values.Add(new KeyValuePair<string, string>("valores[estado]", person.Address.State));
+            values.Add(new KeyValuePair<string, string>("valores[cep]", person.Address.ZipCode.ToString()));
+            values.Add(new KeyValuePair<string, string>("valores[email]", person.Email));
+            values.Add(new KeyValuePair<string, string>("valores[senha]", password));
+            values.Add(new KeyValuePair<string, string>("valores[senha_confirmacao]", password));
+
+            byte[] response = ExtendableType.Post(_configuration["UrlSei"] + "/pmj/cadastro_usuario_externo.php", values);
+
+            var stringResponse = Encoding.GetEncoding(_configuration["SeiEncoding"]).GetString(response);
+            var results = JsonConvert.DeserializeObject<dynamic>(stringResponse);
+
+            if (results.status == 0)
+            {
+                var exception = new InvalidOperationException("Erro ao gerar a sua certificação.");
+                exception.Data.Add("Response string", stringResponse);
+            }
+            else
+            {
+                person.SeiId = results.id_usuario;
+            }
+        }
+
+        /// <summary>
+        /// Updates the field <see cref="EletronicSignatureStatus"/> if user created in the SEI.
+        /// </summary>
+        /// <remarks>
+        /// If situation is <see cref="EletronicSignatureStatus.UnderApproval"/>, does nothing. <see cref="EletronicSignatureStatus.Unsolicited"/>.
+        /// </remarks>
+        public void UpdateEletronicSignatureStatus(Person person)
+        {
+            if (person.SeiId != null)
+            {
+                Person seiPerson = GetSeiPersonBy((long)person.SeiId);
+                if (seiPerson != null)
+                {
+                    if (person.EletronicSignatureStatus == EletronicSignatureStatus.UnderApproval && seiPerson.EletronicSignatureStatus == EletronicSignatureStatus.Unsolicited)
+                    {
+                        return;
+                    }
+
+                    person.EletronicSignatureStatus = seiPerson.EletronicSignatureStatus;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Check if person has approved eletronic signature.
+        /// </summary>
+        /// <remarks>
+        /// Query the SEI situation directly.
+        /// </remarks>
+        /// <returns>True if is approved./returns>
+        public bool SignatureIsAproved(Person person)
+        {
+            if (person.SeiId != null)
+            {
+                Person seiPerson = GetSeiPersonBy((long)person.SeiId);
+                if (seiPerson != null)
+                {
+                    return seiPerson.EletronicSignatureStatus == EletronicSignatureStatus.Approved;
+                }
+            }
+
+            return false;
+        }
+
+        private Person GetSeiPersonBy(string key, string value)
+        {
+            Person person = null;
+
+            var values = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("token", _configuration["Token"]),
+                new KeyValuePair<string, string>(key, value)
+            };
+
+            var response = ExtendableType.Post(_configuration["UrlSei"] + "/pmj/cadastro_usuario_externo_consulta.php", values);
+
+            string strResponse = Encoding.GetEncoding(_configuration["SeiEncoding"]).GetString(response);
+            person = Person.DeserializePersonJson(person, strResponse);
+
+            return person;
         }
     }
 }
